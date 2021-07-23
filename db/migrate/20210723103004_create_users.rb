9cc55a4c261f944e5fe44467b5bb8fb432cb3990
Nickolay Bulavin <bulavinnik@gmail.com>
2021-07-23 14:45:42 +0400

    Create user model

---
+++ db/migrate/20210723103004_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    enable_extension('citext')
+
+    create_table :users do |t|
+      t.string :first_name, null: false
+      t.string :last_name
+      t.citext :email, null: false
+      t.string :password_digest
+
+      t.timestamps
+    end
+  end
+end
